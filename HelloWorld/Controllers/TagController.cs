88bf41875a8700d7d754a030fa21f8e1e47c9c21
SkyHat <skyhat@mail.ru>
2013-12-07 22:20:50 +0300

    Tags v0.2

---
+++ HelloWorld/Controllers/TagController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using HelloWorld.Filters;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    [Culture]
+    [Authorize(Roles = "Admin")]
+    public class TagController : Controller
+    {
+        private HelloWorldDb db = new HelloWorldDb();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.Tags.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tag);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tags.Add(tag);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tag);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tag);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tag).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tag);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tag);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tag tag = db.Tags.Find(id);
+            db.Tags.Remove(tag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
